cc95ca1bcb023ad5f81d0a2596146f47770221e9
skrues <johnmark.business.email@gmail.com>
2025-09-09 18:10:36 +0800

    adding navbar

---
+++ script.js
@@ -0,0 +1,4 @@
+const navbar = document.querySelector(".navbar").addEventListener("click", () => {
+    const headerNav = document.querySelector(".header-nav ul")
+    headerNav.classList.toggle("active")
+})
