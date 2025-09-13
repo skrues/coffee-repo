113711b512f1004bf08b8cf355ced360b4a03532
skrues <johnmark.business.email@gmail.com>
2025-09-13 11:53:11 +0800

    redesign toggle

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const body = document.body
+
 const navbar = document.querySelector(".navbar").addEventListener("click", () => {
     const headerNav = document.querySelector(".header-nav ul")
     headerNav.classList.toggle("active")
+    body.classList.toggle("no-scroll")
 })
+
